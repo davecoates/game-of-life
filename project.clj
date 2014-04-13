b68c3b5a5cd7e2e16de32dab67e3538c94646c1c
Dave Coates <dave@localhost>
2014-04-13 22:10:22 +1000

    Setup for ghpages

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.facebook/react "0.8.0.1"]
                  [org.clojure/clojurescript "0.0-2156"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
                  [om "0.5.0"]]
@@ -12,10 +13,15 @@
   :source-paths ["src"]
 
   :cljsbuild {
-    :builds [{:id "game-of-life"
-              :source-paths ["src"]
-              :compiler {
-                :output-to "game_of_life.js"
-                :output-dir "out"
-                :optimizations :none
-                :source-map true}}]})
+    :builds [
+             {:id "release"
+               :source-paths ["src"]
+               :compiler {
+                          :output-to "main.js"
+                          :optimizations :advanced
+                          :pretty-print false
+                          :preamble ["react/react.min.js"]
+                          :externs ["react/externs/react.js"]
+                          }
+             }
+             ]})
